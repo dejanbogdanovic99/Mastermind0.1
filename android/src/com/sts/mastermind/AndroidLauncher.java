733f4914ff28ad046b9eaa12c0a8888b879dcc03
DekiCar <dejanbogdanovic99@gmail.com>
2017-04-13 14:46:58 +0200

    p1

--- android/src/com/sts/mastermind/AndroidLauncher.java
+++ android/src/com/sts/mastermind/AndroidLauncher.java
@@ -1,15 +1,16 @@
 package com.sts.mastermind;
 
 import android.os.Bundle;
+import android.view.WindowManager;
 
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
-import com.sts.mastermind.Main;
 
 public class AndroidLauncher extends AndroidApplication {
 	@Override
 	protected void onCreate (Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
 		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
 		initialize(new Main(), config);
 	}
